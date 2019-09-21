bd8387282d4ca145f3a5f18f22e684c5b09e3c08
adrian <cr7adrian@hotmail.com>
2019-09-21 04:39:35 -0400

    setting landing page

---
+++ frontend/shelterui/src/components/LandingPage.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="hello">
+    <h2 id="title">Welcome Back!</h2>
+    <div class="form1">
+      <b-form v-on:submit="onSubmit" v-on:reset="onReset">
+        <b-form-group id="input-group-1" label="Email address:" label-for="input-1">
+          <b-form-input
+            id="input-1"
+            v-model="form.email"
+            type="email"
+            required
+            placeholder="Enter email"
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group id="input-group-2" label="Password" label-for="input-2">
+          <b-form-input
+            id="input-2"
+            v-model="form.password"
+            type="password"
+            required
+            placeholder="Enter Password"
+          ></b-form-input>
+        </b-form-group>
+
+        <div id="submit-button">
+          <p><a href="">Forgot Password?</a></p>
+           <b-button type="reset" variant="danger">Sign In</b-button>
+        </div>
+      </b-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LandingPage",
+  data() {
+    return {
+      form: {
+        email: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+      let userData = JSON.stringify(this.form);
+      console.log(userData);
+    },
+  }
+};
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.btn-danger  {
+  background-color:  #3a3986 !important;
+  color: white !important;
+  border-color: #3a3986 !important;
+}
+
+.btn-danger:hover {
+  border-color: #3a3986 !important;
+}
+.hello {
+  background-color: white;
+  border-radius: 5%;
+  left: 35%;
+  top: 25%;
+  position: absolute;
+  bottom: 50%;
+  margin: 0 auto;
+  width: 30%;
+  height: 60%;
+}
+
+a {
+  color: black;
+}
+
+.form1 {
+  padding: 25px;
+  margin-top: 20px;
+}
+
+
+
+#title {
+  padding: 15px;
+  text-align: center;
+  margin-top: 15px;
+  font-size: 35px;
+  font-weight: 500;
+}
+
+</style>
