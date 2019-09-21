db23d7300ce173b038f798b7f0ec228f8a46f317
nvela031 <nvela031@fiu.edu>
2019-09-21 01:47:55 -0400

    I did my first go script

--- backend/main.go
+++ backend/main.go
@@ -4,4 +4,18 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World")
+	a:= print()
+	fmt.Println(a)
+
+	count := 10
+	for index := 0; index < count; index++ {
+		fmt.Println(index)
+	}
+
+
+	fmt.Println(a)
+}
+
+func print()(string){
+	return "Hello Alex"
 }
