d50935bbf459ea997aa0397687b172cd8a151658
adrian <cr7adrian@hotmail.com>
2019-09-21 06:50:39 -0400

    adds dashboard compoent

---
+++ frontend/shelterui/src/components/DashBoard.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="dashboard">
+        <h1>You are in DashBoard</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "DashBoard"
+}
+</script>
+
+<style>
+    #dashboard {
+        background-color: white !important;
+    }
+
+    #app {
+        background-color: white !important;
+    }
+</style>
+
+
