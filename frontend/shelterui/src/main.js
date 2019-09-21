0cd20018e288aa6a2fb263e6ca7ca6afac96e4df
adrian <cr7adrian@hotmail.com>
2019-09-21 07:11:05 -0400

    adds router to the vue instance

--- frontend/shelterui/src/main.js
+++ frontend/shelterui/src/main.js
@@ -5,6 +5,8 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import VueRouter from 'vue-router'
+import DashBoard from './components/DashBoard.vue'
+import LandingPage from './components/LandingPage.vue'
 
 Vue.use(BootstrapVue)
 Vue.use(VueRouter)
@@ -12,6 +14,16 @@ Vue.use(VueRouter)
 Vue.config.productionTip = false
 
 
+const router = new VueRouter({
+  mode: "history",
+  routes: [
+    {path:'/', component: LandingPage},
+    {path:'/dashboard', component: DashBoard},
+    // {path: '/projects', component: ProjectBox, props: true}
+  ] // short for `routes: routes`
+});
+
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
