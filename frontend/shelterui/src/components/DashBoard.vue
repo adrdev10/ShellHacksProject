ed0b5e2d769fc858ef8dafda597486eef89104df
adrian <cr7adrian@hotmail.com>
2019-09-22 06:25:34 -0400

    changes to the server

--- frontend/shelterui/src/components/DashBoard.vue
+++ frontend/shelterui/src/components/DashBoard.vue
@@ -95,8 +95,9 @@ export default {
   watch: {
     capacity: function(newVal, oldVal) {
       console.log(newVal);
-      fetch("http://localhost:3000/getCalls", {
-        method:'get',
+      fetch("http://localhost:3000/postCalls", {
+        method:'post',
+        body: this.capacity,
       }).then((res, err) => {
         if (err) {
           console.log("Problem with server")
