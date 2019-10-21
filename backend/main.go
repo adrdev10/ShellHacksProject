1a6387d651ddc0e24b1de1fb24f39b4e248cd887
nvela031 <nvela031@fiu.edu>
2019-10-20 21:12:14 -0400

    adding these files

--- backend/main.go
+++ backend/main.go
@@ -2,45 +2,41 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"log"
-    "net/http"
+	"net/http"
 )
 
-type Page struct {
-	Title string
-	Body  []byte
-}
-
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return ioutil.WriteFile(filename, p.Body, 0600)
-}
-
-func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	return &Page{Title: title, Body: body}, nil
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
-}
-
-
 func main() {
-	http.HandleFunc("/", handler)
-    log.Fatal(http.ListenAndServe(":8080", nil))
-	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-	p1.save()
-	p2, _ := loadPage("TestPage")
-	fmt.Println(string(p2.Body))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "This is a website server by a Go HTTP server.")
+	})
+
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World! I'm a HTTP server!")
+	})
+
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.ListenAndServe(":3001", nil)
+
+	// ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	// client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb+srv://devconnector-yhovu.mongodb.net/test"))
+	// // if err != nil {
+	// // 	panic(err)
+	// // }
+	// // err = client.Ping(ctx, readpref.Primary())
+
+	// ctx, _ = context.WithTimeout(context.Background(), 2*time.Second)
+	// err = client.Ping(ctx, readpref.Primary())
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// collection := client.Database("testing").Collection("data")
+	// ctx, _ = context.WithTimeout(context.Background(), 5*time.Second)
+	// res, err := collection.InsertOne(ctx, bson.M{"name": "pi", "value": 3001})
+	// // if err != nil {
+	// // 	fmt.Println(err)
+	// // }
+	// id := res.InsertedID
+	// fmt.Println(id)
 }
-
-
-// func print()(string){
-// 	return "Hello Alex"
-// }
